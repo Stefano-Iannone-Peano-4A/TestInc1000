f38733d09d691ac8d725feaf8c0384efb403d2af
Stefano-Iannone-Peano-4A <56471045+Stefano-Iannone-Peano-4A@users.noreply.github.com>
2019-11-30 10:26:02 +0100

    Aggiunti file java
    Aggiunti file java del programma TestInc1000.

---
+++ Inc1000.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testinc1000;
+
+/**
+ *
+ * @author iannone.stefano
+ */
+public class Inc1000 extends Thread{
+    static int cont;
+    
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            cont++;
+            
+        }
+    }
+}
---
+++ TestInc1000.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testinc1000;
+
+/**
+ *
+ * @author iannone.stefano
+ */
+public class TestInc1000 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Inc1000 t1 = new Inc1000();
+        Inc1000 t2 = new Inc1000();
+        t1.start();
+        t2.start();
+        System.out.println(Inc1000.cont);
+    }
+    
+}
